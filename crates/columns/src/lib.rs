eac695134218f5b2cd452162740086491f07b7ba
Georg Wiese <georgwiese@gmail.com>
2025-01-07 00:25:25 +0100

    ChatGPT refactor + implement GenericArray

--- crates/columns/src/lib.rs
+++ crates/columns/src/lib.rs
@@ -18,132 +18,18 @@ pub fn flatten_fields(input: TokenStream) -> TokenStream {
     // Get the generic type `T` if it exists
     let generic_type = generics.type_params().next().expect("Expected a generic parameter");
 
+    // Generate code to flatten fields
     let field_list_code = match input.data {
         Data::Struct(data_struct) => match data_struct.fields {
             Fields::Named(fields) => {
-                fields.named.iter().map(|field| {
-                    let field_name = field.ident.as_ref().unwrap().to_string();
-                    let field_type = &field.ty;
-
-                    // Check if the field type is the same as the generic type `T`
-                    if let syn::Type::Path(type_path) = field_type {
-                        if type_path.path.is_ident(&generic_type.ident) {
-                            // This field is of type `T`, treat it as a terminal field
-                            return quote! {
-                                fields.push(#field_name.to_string());
-                            };
-                        }
-                    }
-
-                    // Handle array types `[T; N]`
-                    if let syn::Type::Array(array_type) = field_type {
-                        let elem_type = &*array_type.elem; // Dereference the Box to get the inner type
-                        let array_len = &array_type.len;
-
-                        // Check if the array element type is `T`
-                        if let syn::Type::Path(type_path) = elem_type {
-                            if type_path.path.is_ident(&generic_type.ident) {
-                                // Array of `T`, treat as terminal
-                                return quote! {
-                                    for i in 0..#array_len {
-                                        fields.push(format!("{}__{}", #field_name, i));
-                                    }
-                                };
-                            }
-                        }
-
-                        // Otherwise, recursively process the array elements
-                        return quote! {
-                            if let Some(sub_fields) = <#elem_type as FlattenFieldsHelper>::flatten_fields() {
-                                for i in 0..#array_len {
-                                    for sub_field in &sub_fields {
-                                        fields.push(format!("{}__{}__{}", #field_name, i, sub_field));
-                                    }
-                                }
-                            } else {
-                                for i in 0..#array_len {
-                                    fields.push(format!("{}__{}", #field_name, i));
-                                }
-                            }
-                        };
-                    }
-
-                    // For non-generic, non-array types, use the existing logic
-                    quote! {
-                        if <#field_type as FlattenFieldsHelper>::flatten_fields().is_some() {
-                            if let Some(sub_fields) = <#field_type as FlattenFieldsHelper>::flatten_fields() {
-                                for sub_field in sub_fields {
-                                    fields.push(format!("{}__{}", #field_name, sub_field));
-                                }
-                            }
-                        } else {
-                            fields.push(#field_name.to_string());
-                        }
-                    }
-                }).collect::<Vec<_>>()
+                fields.named.iter().map(|field| generate_field_code(field, &generic_type)).collect()
             }
-            Fields::Unnamed(fields) => {
-                fields.unnamed.iter().enumerate().map(|(i, field)| {
-                    let index = syn::Index::from(i);
-                    let field_type = &field.ty;
-
-                    // Check if the field type is the same as the generic type `T`
-                    if let syn::Type::Path(type_path) = field_type {
-                        if type_path.path.is_ident(&generic_type.ident) {
-                            // This field is of type `T`, treat it as a terminal field
-                            return quote! {
-                                fields.push(format!("{}", #index));
-                            };
-                        }
-                    }
-
-                    // Handle array types `[T; N]`
-                    if let syn::Type::Array(array_type) = field_type {
-                        let elem_type = &*array_type.elem; // Dereference the Box to get the inner type
-                        let array_len = &array_type.len;
-
-                        // Check if the array element type is `T`
-                        if let syn::Type::Path(type_path) = elem_type {
-                            if type_path.path.is_ident(&generic_type.ident) {
-                                // Array of `T`, treat as terminal
-                                return quote! {
-                                    for i in 0..#array_len {
-                                        fields.push(format!("{}__{}", #index, i));
-                                    }
-                                };
-                            }
-                        }
-
-                        // Otherwise, recursively process the array elements
-                        return quote! {
-                            if let Some(sub_fields) = <#elem_type as FlattenFieldsHelper>::flatten_fields() {
-                                for i in 0..#array_len {
-                                    for sub_field in &sub_fields {
-                                        fields.push(format!("{}__{}__{}", #index, i, sub_field));
-                                    }
-                                }
-                            } else {
-                                for i in 0..#array_len {
-                                    fields.push(format!("{}__{}", #index, i));
-                                }
-                            }
-                        };
-                    }
-
-                    // For non-generic, non-array types, use the existing logic
-                    quote! {
-                        if <#field_type as FlattenFieldsHelper>::flatten_fields().is_some() {
-                            if let Some(sub_fields) = <#field_type as FlattenFieldsHelper>::flatten_fields() {
-                                for sub_field in sub_fields {
-                                    fields.push(format!("{}__{}", #index, sub_field));
-                                }
-                            }
-                        } else {
-                            fields.push(format!("{}", #index));
-                        }
-                    }
-                }).collect::<Vec<_>>()
-            }
+            Fields::Unnamed(fields) => fields
+                .unnamed
+                .iter()
+                .enumerate()
+                .map(|(i, field)| generate_unnamed_field_code(i, field, &generic_type))
+                .collect(),
             Fields::Unit => vec![],
         },
         _ => panic!("FlattenFields can only be used on structs."),
@@ -160,4 +46,180 @@ pub fn flatten_fields(input: TokenStream) -> TokenStream {
     };
 
     TokenStream::from(expanded)
+}
+
+fn generate_field_code(
+    field: &syn::Field,
+    generic_type: &syn::TypeParam,
+) -> proc_macro2::TokenStream {
+    let field_name = field.ident.as_ref().unwrap().to_string();
+    let field_type = &field.ty;
+
+    // Check if the field type is `T`
+    if let syn::Type::Path(type_path) = field_type {
+        if type_path.path.is_ident(&generic_type.ident) {
+            return quote! {
+                fields.push(#field_name.to_string());
+            };
+        }
+    }
+
+    // Handle `GenericArray<ElementType, Size>`
+    if let syn::Type::Path(type_path) = field_type {
+        if type_path.path.segments.last().unwrap().ident == "GenericArray" {
+            if let syn::PathArguments::AngleBracketed(args) =
+                &type_path.path.segments.last().unwrap().arguments
+            {
+                let element_type = &args.args[0];
+                let size_type = &args.args[1];
+
+                return quote! {
+                    if let Some(sub_fields) = <#element_type as FlattenFieldsHelper>::flatten_fields() {
+                        for i in 0..<#size_type as typenum::Unsigned>::USIZE {
+                            for sub_field in &sub_fields {
+                                fields.push(format!("{}__{}__{}", #field_name, i, sub_field));
+                            }
+                        }
+                    } else {
+                        for i in 0..<#size_type as typenum::Unsigned>::USIZE {
+                            fields.push(format!("{}__{}", #field_name, i));
+                        }
+                    }
+                };
+            }
+        }
+    }
+
+    // Handle array types `[T; N]`
+    if let syn::Type::Array(array_type) = field_type {
+        let elem_type = &*array_type.elem; // Dereference the Box to get the inner type
+        let array_len = &array_type.len;
+
+        // Check if the array element type is `T`
+        if let syn::Type::Path(type_path) = elem_type {
+            if type_path.path.is_ident(&generic_type.ident) {
+                return quote! {
+                    for i in 0..#array_len {
+                        fields.push(format!("{}__{}", #field_name, i));
+                    }
+                };
+            }
+        }
+
+        // Otherwise, recursively process the array elements
+        return quote! {
+            if let Some(sub_fields) = <#elem_type as FlattenFieldsHelper>::flatten_fields() {
+                for i in 0..#array_len {
+                    for sub_field in &sub_fields {
+                        fields.push(format!("{}__{}__{}", #field_name, i, sub_field));
+                    }
+                }
+            } else {
+                for i in 0..#array_len {
+                    fields.push(format!("{}__{}", #field_name, i));
+                }
+            }
+        };
+    }
+
+    // For other types
+    quote! {
+        if <#field_type as FlattenFieldsHelper>::flatten_fields().is_some() {
+            if let Some(sub_fields) = <#field_type as FlattenFieldsHelper>::flatten_fields() {
+                for sub_field in sub_fields {
+                    fields.push(format!("{}__{}", #field_name, sub_field));
+                }
+            }
+        } else {
+            fields.push(#field_name.to_string());
+        }
+    }
+}
+
+fn generate_unnamed_field_code(
+    index: usize,
+    field: &syn::Field,
+    generic_type: &syn::TypeParam,
+) -> proc_macro2::TokenStream {
+    let field_type = &field.ty;
+
+    // Check if the field type is `T`
+    if let syn::Type::Path(type_path) = field_type {
+        if type_path.path.is_ident(&generic_type.ident) {
+            return quote! {
+                fields.push(format!("{}", #index));
+            };
+        }
+    }
+
+    // Handle `GenericArray<ElementType, Size>`
+    if let syn::Type::Path(type_path) = field_type {
+        if type_path.path.segments.last().unwrap().ident == "GenericArray" {
+            if let syn::PathArguments::AngleBracketed(args) =
+                &type_path.path.segments.last().unwrap().arguments
+            {
+                let element_type = &args.args[0];
+                let size_type = &args.args[1];
+
+                return quote! {
+                    if let Some(sub_fields) = <#element_type as FlattenFieldsHelper>::flatten_fields() {
+                        for i in 0..<#size_type as typenum::Unsigned>::USIZE {
+                            for sub_field in &sub_fields {
+                                fields.push(format!("{}__{}__{}", #index, i, sub_field));
+                            }
+                        }
+                    } else {
+                        for i in 0..<#size_type as typenum::Unsigned>::USIZE {
+                            fields.push(format!("{}__{}", #index, i));
+                        }
+                    }
+                };
+            }
+        }
+    }
+
+    // Handle array types `[T; N]`
+    if let syn::Type::Array(array_type) = field_type {
+        let elem_type = &*array_type.elem; // Dereference the Box to get the inner type
+        let array_len = &array_type.len;
+
+        // Check if the array element type is `T`
+        if let syn::Type::Path(type_path) = elem_type {
+            if type_path.path.is_ident(&generic_type.ident) {
+                return quote! {
+                    for i in 0..#array_len {
+                        fields.push(format!("{}__{}", #index, i));
+                    }
+                };
+            }
+        }
+
+        // Otherwise, recursively process the array elements
+        return quote! {
+            if let Some(sub_fields) = <#elem_type as FlattenFieldsHelper>::flatten_fields() {
+                for i in 0..#array_len {
+                    for sub_field in &sub_fields {
+                        fields.push(format!("{}__{}__{}", #index, i, sub_field));
+                    }
+                }
+            } else {
+                for i in 0..#array_len {
+                    fields.push(format!("{}__{}", #index, i));
+                }
+            }
+        };
+    }
+
+    // For other types
+    quote! {
+        if <#field_type as FlattenFieldsHelper>::flatten_fields().is_some() {
+            if let Some(sub_fields) = <#field_type as FlattenFieldsHelper>::flatten_fields() {
+                for sub_field in sub_fields {
+                    fields.push(format!("{}__{}", #index, sub_field));
+                }
+            }
+        } else {
+            fields.push(format!("{}", #index));
+        }
+    }
 }
